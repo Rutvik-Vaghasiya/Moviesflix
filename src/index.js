2650a4db770f125c6441a284274aadf8390d8ade
Meet <meetbhalodiya1030@gmail.com>
2022-04-08 17:03:24 +0530

    use latest approach

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./styles/index.scss";
 import App from "./App";
 import { Provider } from "./components/Functions/context";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
 	<React.StrictMode>
 		<Provider>
 			<App />
 		</Provider>
-	</React.StrictMode>,
-	document.getElementById("root")
+	</React.StrictMode>
 );
